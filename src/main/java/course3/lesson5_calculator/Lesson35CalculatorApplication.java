b3130d4b961e9c8d1218b37344b58252b1cd99f1
Ирина <mi.irene97@gmail.com>
2022-11-24 18:48:25 +0300

    Initial commit

---
+++ src/main/java/course3/lesson5_calculator/Lesson35CalculatorApplication.java
@@ -0,0 +1,13 @@
+package course3.lesson5_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson35CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson35CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course3/lesson5_calculator/Lesson35CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package course3.lesson5_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson35CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
